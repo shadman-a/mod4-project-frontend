feb3bc0913c239c3e4b845146f18750a9042725d
rrosenb12 <rebecca.ap.rosenberg@gmail.com>
2020-08-19 10:30:07 -0400

    hi

--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -29,13 +29,23 @@ class Profile extends React.Component{
         fetch(`http://localhost:3000/users/${this.props.user.id}`,{ method: "DELETE"}
     )}
 
+    getHighScore = () => {
+        if (this.props.user.high_score === 0 || this.props.user.high_score === null) {
+            return <h2>Your High Score is: 0</h2>
+        } else if (this.state.score > this.state.high_score) {
+            return <h2>Your High Score is: {this.state.score}</h2>
+        } else {
+            return <h2>Your High Score is: {this.state.high_score}</h2>
+        }
+    }
+
     render(){
         return(
         <div className="welcomecontainer">
         <h1>Game Over</h1>
         <h1>Hi, {this.state.user} </h1>
-        {this.state.high_score === 0 || this.state.high_score === null ? <h2>Your High Score is: 0</h2> : <h2>Your High Score is: {this.props.user.high_score}</h2>}
-        
+        {/* {this.props.user.high_score === 0 || this.props.user.high_score === null ? <h2>Your High Score is: 0</h2> : <h2>Your High Score is: {this.props.user.high_score}</h2>} */}
+            {this.getHighScore()}
         <h3>Your most recent score is: {this.state.score}</h3>
         <NavLink style={{color: 'white', textDecoration: 'none', backgroundColor: 'red', padding: '14px 25px'}} to='/login' onClick={this.deleteHandler}>Delete</NavLink>
         </div>
